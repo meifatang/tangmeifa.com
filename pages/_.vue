b1bd94a2e4017364016664858a177af7d586b99c
meifatang <me@tangmeifa.com>
2021-06-06 08:28:23 +0800

    update

--- pages/_.vue
+++ pages/_.vue
@@ -1,17 +1,16 @@
 <template>
   <article>
-    <h1>
-        <a v-bind:href="article.path">{{ article.title }}</a>
-    </h1>
-    <br />
+    <h1>{{ article.title }}</h1>
+    <hr />
+    <nuxt-content :document="article" />
   </article>
 </template>
 
 <script>
 export default {
   async asyncData ({ $content, app, params, error }) {
-    //const path = `/${params.pathMatch || 'index'}`
-    const [article] = await $content({ deep: true }).only(['title']).fetch()
+    const path = `/${params.pathMatch || 'index'}`
+    const [article] = await $content({ deep: true }).where({ path }).fetch()
 
     if (!article) {
       return error({ statusCode: 404, message: 'Article not found' })
---
+++ pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="">
+        About Page
+    </div>
+</template>
