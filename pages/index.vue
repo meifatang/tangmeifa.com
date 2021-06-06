697ce8b08c62553bec407c4244adaf563ec717ec
meifatang <me@tangmeifa.com>
2021-06-06 08:45:26 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,8 @@
       </h1>
       <h2 class="">Personal Website</h2>
       <div class="">
-        <a class="" href="https://blog.tangmeifa.com">中文博客</a>
+        <a class="" href="/blog/">Blog</a>
+        <a class="" href="https://blog.tangmeifa.com">博客</a>
       </div>
     </div>
   </div>
